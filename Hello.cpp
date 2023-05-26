b066cd4b4f37b85e49812c7895aad0507d12de26
zawa-tin <contactaizawa@icloud.com>
2023-05-26 13:43:07 +0900

    firmatter

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+  std::cout <<"Hello World"<< std::endl;
+  for (int i = 0 ; i < 10 ; i++) cout << i << (i == 9 ? '\n' : ' ');
+  return 0;
 }
