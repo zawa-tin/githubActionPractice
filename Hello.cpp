9e4182d150678e59952d0bebf6dca5dac7789bb2
zawa-tin <contactaizawa@icloud.com>
2023-05-26 14:01:52 +0900

    uku

--- Hello.cpp
+++ Hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-  std::cout <<"Hello World"<< std::endl;
-  for (int i = 0 ; i < 10 ; i++) cout << i << (i == 9 ? '\n' : ' ');
+  std::cout << "Hello World" << std::endl;
+  for (int i = 0; i < 10; i++) cout << i << (i == 9 ? '\n' : ' ');
   return 0;
 }
